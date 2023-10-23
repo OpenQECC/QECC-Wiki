ee8ac1137c50ffa109ac77d6177557184c8e4c24
Zhiheng Wang <zhihengwang@umass.edu>
2023-10-22 20:12:41 -0400

    include the table decoder for errors up to 3 as well as giving a syndrome for Steane-7 code

---
+++ src/generator/util/table_decoder.jl
@@ -0,0 +1,45 @@
+using DataStructures
+using Combinatorics
+
+
+function create_lookup_table_3_errors(H)
+    lookup_table_three = Dict()
+    constraints, bits = size(H)
+    
+    lookup_table_three[zeros(Int, constraints)] = zeros(Int, bits)
+
+    for num_errors in 1:3  # Looping for 1 to 3 errors
+        # This function will generate combinations of positions where errors occur
+        for error_positions in combinations(1:bits, num_errors)
+            error = zeros(Int, bits)
+            for pos in error_positions
+                error[pos] = 1
+            end
+            syndrome = (H * error) .% 2
+            lookup_table_three[syndrome] = error
+        end
+    end
+    
+    return lookup_table_three
+end
+
+function decode_using_lookup_table(syndrome, lookup_table)
+    if haskey(lookup_table, syndrome)
+        return lookup_table[syndrome]
+    else
+        error("Unknown syndrome encountered.")
+    end
+end
+
+# Steane-7 Code's Parity Check Matrix
+H = [1 0 0 1 0 1 1; 
+     0 1 0 1 1 0 1; 
+     0 0 1 1 1 1 0];
+
+lookup_table_three = create_lookup_table_3_errors(H)
+
+# For demonstration purposes, let's consider a syndrome
+measured_syndrome = [1, 0, 0]  # This is just a sample.
+
+decoded_error = decode_using_lookup_table(measured_syndrome, lookup_table_three)
+println(decoded_error)
